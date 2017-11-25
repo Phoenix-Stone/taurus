7c6d4099aa12f1d98e4014a29bc2e290669e8158
wenlong <orangespy@163.com>
2017-11-25 12:26:54 +0800

    配置修改
    eslint-self

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,5 +23,10 @@ module.exports = {
     'generator-star-spacing': 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'semi':['error','always'],
+    'indent':0,
+    'space-before-function-paren':0,
+    'no-unused-vars': ['warn', { 'vars': 'all', 'args': 'after-used', 'ignoreRestSiblings': false }],
+    'no-multiple-empty-lines': ['warn', { 'max': 4}]
   }
 }
